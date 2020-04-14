cb62fb7b7e93db00e54fd1489ef8aa826b2d48f1
Gina Holden <gina.m.holden@gmail.com>
2020-04-14 08:11:16 -0400

    Adds tests for chapter 2 loop detection

--- Chapter2/8_Loop_Detection.py
+++ Chapter2/8_Loop_Detection.py
@@ -1,3 +1,16 @@
+# given a circular linked list, implement an algorithm that returns the node at the beginning of the loop
+# definition: curcular linked list: a (corrupt) linked list in which a node's next pointer points to an 
+# earlier node, so as to make aloop inthe linked list. 
+# example: A -> B-> C -> D -> E -----
+#                   ^                |
+#                   |________________|
+# output: C
+
+# General algorithm: if the un-looped part of the list (example A and B) is K nodes long, use a fast pointer and a slow pointer. 
+# When the two pointers meet, they will be K nodes away from the start of the loop. Then you move one of the nodes to the start of the list
+# and when they meet again they will be at the start of the loop.
+import unittest
+
 from LinkedList import LinkedList
 
 
@@ -19,3 +32,22 @@ def loop_detection(ll):
         slow = slow.next
 
     return fast
+
+
+class Test(unittest.TestCase):
+  # example: 1 -> 2 -> 3 -> 4 -> 5 -----
+#                    ^                |
+#                    |________________|
+
+    def test_loop_detection(self):
+      linked_list = LinkedList([1, 2, 3, 4, 5])
+      three_node = linked_list.head.next.next
+      linked_list.tail.next = three_node
+      linked_list.tail = None
+      loop_start_node = loop_detection(linked_list)
+      self.assertEqual(three_node, loop_start_node)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
