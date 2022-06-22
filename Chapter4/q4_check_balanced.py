e7c4f2aa1c1d3d400b47466754eb82737b7bac42
Gina Holden <gina.m.holden@gmail.com>
2022-06-22 08:16:54 -0400

    fixes chap 4 q 4 check balanced

--- Chapter4/q4_check_balanced.py
+++ Chapter4/q4_check_balanced.py
@@ -4,96 +4,103 @@
 
 
 import unittest
-from binary_tree import BinaryTree, BinaryTreeNode
+from Chapter4.binary_tree import BinaryTreeNode
+
 ERROR = -3
 
-def check_height(node: BinaryTreeNode) -> bool:
-  if node is None:
-    return -1
-  left_height = check_height(node.left)
-  if left_height is ERROR:
-    return ERROR
-  
-  right_height = check_height(node.right)
-  if right_height is ERROR:
-    return ERROR
-  
-  height_diff = left_height - right_height
-  if abs(height_diff) > 1:
-    return ERROR
-  else:
-    return max(left_height, right_height) + 1
+
+def check_height(node: BinaryTreeNode) -> int:
+    if node is None:
+        return -1
+    left_height = check_height(node.left)
+    if left_height is ERROR:
+        return ERROR
+
+    right_height = check_height(node.right)
+    if right_height is ERROR:
+        return ERROR
+
+    height_diff = left_height - right_height
+    if abs(height_diff) > 1:
+        return ERROR
+    else:
+        return max(left_height, right_height) + 1
+
 
 def check_balanced(binary_tree_node: BinaryTreeNode) -> bool:
-  return check_height(binary_tree_node) is not ERROR
+    return check_height(binary_tree_node) != -3
 
-def run_check_balanced( binary_tree: BinaryTree) -> bool: 
-  return check_balanced(binary_tree.root)
 
 class Test(unittest.TestCase):
-  def test_balanced_true_perfect(self):
+    def test_balanced_true_perfect(self):
+        #          1
+        #       /     \
+        #      2        3
+        #    /   \     / \
+        #   4     5   6    7
+        six_node = BinaryTreeNode("6")
+        seven_node = BinaryTreeNode("7")
+        three_node = BinaryTreeNode("3")
+        three_node.left = six_node
+        three_node.right = seven_node
+        four_node = BinaryTreeNode("4")
+        five_node = BinaryTreeNode("5")
+        two_node = BinaryTreeNode("2")
+        two_node.left = four_node
+        two_node.right = five_node
+        one_node = BinaryTreeNode("1")
+        one_node.left = two_node
+        one_node.right = three_node
+        balanced = check_balanced(one_node)
+        self.assertTrue(balanced)
 
-#          1
-#       /     \
-#      2        3
-#    /   \     / \ 
-#   4     5   6    7
-    six_node = BinaryTreeNode("6")
-    seven_node = BinaryTreeNode("7")
-    three_node = BinaryTreeNode("3")
-    three_node.left = six_node
-    three_node.right = seven_node
-    four_node = BinaryTreeNode("4")
-    five_node = BinaryTreeNode("5")
-    two_node = BinaryTreeNode("2")
-    two_node.left = four_node
-    two_node.right = five_node
-    one_node = BinaryTreeNode("1")
-    one_node.left = two_node
-    one_node.right = three_node
-    binary_tree = BinaryTree(one_node)
-    balanced = run_check_balanced(binary_tree)
-    self.assertTrue(balanced)
+    def test_balanced_true(self):
+        #          1
+        #       /     \
+        #      2        3
+        #    /   \     /
+        #   4     5   6
+        six_node = BinaryTreeNode("6")
+        three_node = BinaryTreeNode("3")
+        three_node.left = six_node
+        four_node = BinaryTreeNode("4")
+        five_node = BinaryTreeNode("5")
+        two_node = BinaryTreeNode("2")
+        two_node.left = four_node
+        two_node.right = five_node
+        one_node = BinaryTreeNode("1")
+        one_node.left = two_node
+        one_node.right = three_node
+        balanced = check_balanced(one_node)
+        self.assertTrue(balanced)
 
-  def test_balanced_true(self):
-  
-#          1
-#       /     \
-#      2        3
-#    /   \     / 
-#   4     5   6    
-    six_node = BinaryTreeNode("6")
-    three_node = BinaryTreeNode("3", six_node)
-    four_node = BinaryTreeNode("4")
-    five_node = BinaryTreeNode("5")
-    two_node = BinaryTreeNode("2", four_node, five_node)
-    one_node = BinaryTreeNode("1", two_node, three_node)
-    binary_tree = BinaryTree(one_node)
-    balanced = run_check_balanced(binary_tree)
-    self.assertTrue(balanced)
-
-  def test_balanced_false(self):
-    
-#          1
-#       /     \
-#      2        3
-#    /   \     / 
-#   4     5   6  
-#         |
-#         7  
-#         |
-#         8
-    six_node = BinaryTreeNode("6")
-    three_node = BinaryTreeNode("3", six_node)
-    four_node = BinaryTreeNode("4")
-    eight_node = BinaryTreeNode("8")
-    seven_node = BinaryTreeNode("7", eight_node)
-    five_node = BinaryTreeNode("5", seven_node)
-    two_node = BinaryTreeNode("2", four_node, five_node)
-    one_node = BinaryTreeNode("1", two_node, three_node)
-    binary_tree = BinaryTree(one_node)
-    balanced = run_check_balanced(binary_tree)
-    self.assertFalse(balanced)
+    def test_balanced_false(self):
+        #          1
+        #       /     \
+        #      2        3
+        #    /   \     /
+        #   4     5   6
+        #         |
+        #         7
+        #         |
+        #         8
+        six_node = BinaryTreeNode("6")
+        three_node = BinaryTreeNode("3")
+        three_node.left = six_node
+        four_node = BinaryTreeNode("4")
+        eight_node = BinaryTreeNode("8")
+        seven_node = BinaryTreeNode("7")
+        seven_node.left = eight_node
+        five_node = BinaryTreeNode("5")
+        five_node.left = seven_node
+        two_node = BinaryTreeNode("2")
+        two_node.left = four_node
+        two_node.right = five_node
+        one_node = BinaryTreeNode("1")
+        one_node.left = two_node
+        one_node.right = three_node
+        balanced = check_balanced(one_node)
+        self.assertFalse(balanced)
 
 
 if __name__ == "__main__":
---
+++ Chapter4/q6_successor.py
@@ -0,0 +1,2 @@
+# write an algorithm to find the "next" node (ie in-order successor) of a given node in a bst.
+# you may assume that each node has a link to its parent
