a44282431abddbabfb007a55194368e75acb28c4
Gina Holden <gina.m.holden@gmail.com>
2022-06-25 18:04:34 -0400

    adds bucket sort for floats with integers

--- Chapter10/bucket_sort.py
+++ Chapter10/bucket_sort.py
@@ -43,6 +43,43 @@ def bucket_sort(arr, n):
             arr[index] = buckets[i][j]
             index += 1
 
+
+def bucket_int(arr, n):
+    if n <= 0:
+        return
+
+    # 1) Create n empty buckets
+    buckets = [0] * n
+
+    for i in range( n):
+        buckets[i] = []
+
+    # 2) Put array elements in different buckets
+    m_a = min(arr)
+    r = (max(arr) - m_a) / n
+
+
+    for i in range( n):
+        idx = int(math.floor((arr[i] - m_a) / r))
+        diff = (arr[i] - m_a) / r - int((arr[i] - m_a) / r)
+        # append the boundary elements to the lower array
+        if diff == 0 and arr[i] != m_a:
+            buckets[idx - 1].append(arr[i])
+
+        else:
+            buckets[idx].append(arr[i])
+
+    # 3) Sort individual buckets
+    # Sort individual buckets
+    for i in range(n):
+        buckets[i] = insertion_sort(buckets[i])
+    # 4) Concatenate all buckets into arr[]
+    index = 0
+    for i in range(n):
+        for j in range(len(buckets[i])):
+            arr[index] = buckets[i][j]
+            index += 1
+
 class Test(unittest.TestCase):
     def test_bs(self):
         arr =[0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
@@ -50,3 +87,9 @@ class Test(unittest.TestCase):
         bucket_sort(arr, n)
         print(arr)
         self.assertEqual(arr, [ 0.1234, 0.3434, 0.565, 0.656, 0.665, 0.897 ])
+    def test_bs_ints(self):
+        arr = [6, 4, 3, 8, 9, 7]
+        n = len(arr)
+        bucket_int(arr, n)
+        self.assertEqual(arr, [3, 4, 6, 7, 8, 9])
+
